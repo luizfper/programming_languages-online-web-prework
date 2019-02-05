a86a34ec9faea4e0834d9a8b0860adf8400592a6
Luiz Pereira <luizfper@gmail.com>
2019-02-05 01:59:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,37 @@
 def reformat_languages(languages)
-  # your code here
+  mainhash=main_hash
+end
+
+def main_hash
+ main_hash=languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+ }
 end
