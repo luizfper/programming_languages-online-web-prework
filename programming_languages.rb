7d78e4b8a80e917f75b418814c86fc9981798bb0
Luiz Pereira <luizfper@gmail.com>
2019-02-05 02:12:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages
   new_hash={}
   mainhash.each do |style,language|
     language.each do |language2, type|
-      new_hash[language2]={type},{style:style}
+      new_hash[language2]={{type},{style:style}}
     end
   end
   puts new_hash
